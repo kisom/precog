b0b26a893d878a16dae1bb9d16b4ba5dd7b51e4b
Kyle Isom <coder@kyleisom.net>
2012-11-19 03:26:07 -0700

    Update autoconf infrastrucure
    * add prebuild.sh to automatically call autoreconf
    * add regen.sh to automatically reconfigure
      - OpenBSD and OS X use /usr/local/lib for locally installed libraries
    * change project name to voltaire
    * update CFLAGS in source Makefile to include local include files

---
+++ c/regen.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo "[+] rebuilding"
+./prebuild.sh
+
+OSNAME="$(uname -s)"
+if [ "x${OSNAME}" = "xOpenBSD" ]; then
+        echo "[+] configuring for ${OSNAME}"
+        ./configure LDFLAGS=-L/usr/local/lib
+elif [ "x${OSNAME}" = "xDarwin" ]; then
+        echo "[+] configuring for ${OSNAME}"
+        ./configure LDFLAGS=-L/usr/local/lib
+else
+        echo "[+] using generic configuration"
+        ./configure
+fi
