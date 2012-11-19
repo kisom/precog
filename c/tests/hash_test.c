55b63f89df790f8803299784a646691b60225661
Kyle Isom <coder@kyleisom.net>
2012-11-19 04:58:49 -0700

    adding tests

---
+++ c/tests/hash_test.c
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2012 Kyle Isom <kyle@tyrfingr.is>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+ * WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+ * AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+ * DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA
+ * OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ * ---------------------------------------------------------------------
+ */
+
+
+#include <sys/types.h>
+#include <CUnit/CUnit.h>
+#include <CUnit/Basic.h>
+#include <err.h>
+#include <stdio.h>
+#include <sysexits.h>
+
+#include "hash.h"
+
+
+/*
+ * Suite set up and tear down functions.
+ */
+int
+initialise_hash_test()
+{
+        return 0;
+}
+
+int
+cleanup_hash_test()
+{
+        return 0;
+}
+
+void destroy_test_registry()
+{
+        CU_cleanup_registry();
+        exit(CU_get_error);
+}
+
+
+/*
+ * Ensure that the hex digest creates the appropriate value; this
+ * will test both the hex digest and binary digest (as the binary
+ * digest is part of the hex digest code).
+ */
+void
+test_hello_world()
+{
+        int rv;
+        char hw[] = "hello, world";
+        char hw_hexhash[] = "b7e23ec29af22b0b4e41da31e868d57226121c84";
+        char *hash = NULL;
+
+        printf("[-] strlen(hw) -> %u\n", (unsigned int)strlen(hw));
+        rv = voltaire_hexhash_data(hw, strlen(hw), (uint8_t *)hash);
+        CU_ASSERT(rv == EXIT_SUCCESS);
+
+        CU_ASSERT(0 == strncmp(hw, hash, strlen(hw)));
+        free(hash);
+}
+
+
+/*
+ * hash_test runs checks to ensure the hashing code works properly.
+ */
+int
+main(void)
+{
+        CU_pSuite hash_suite = NULL;
+        unsigned int fails = 0;
+
+        printf("[+] running tests for hash functions.\n");
+
+        if (!(CUE_SUCCESS == CU_initialize_registry())) {
+                errx(EX_CONFIG, "failed to initialise CUnit test registry");
+                return EXIT_FAILURE;
+        }
+
+        hash_suite = CU_add_suite("hash_tests", initialise_hash_test,
+            cleanup_hash_test);
+        if (NULL == hash_suite)
+                destroy_test_registry();
+
+        if (NULL == CU_add_test(hash_suite, "hello world test",
+                    test_hello_world))
+                destroy_test_registry();
+
+        CU_basic_set_mode(CU_BRM_VERBOSE);
+        CU_basic_run_tests();
+        fails = CU_get_number_of_tests_failed();
+        printf("[+] %u tests failed\n", fails);
+
+        CU_cleanup_registry();
+        return fails;
+}
